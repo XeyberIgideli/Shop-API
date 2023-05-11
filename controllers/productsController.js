4b57f7dd7a908673624ae9fa1f6cc6ebc91983db
XeyberIgideli <d3facerx@gmail.com>
2023-05-11 12:50:23 +0400

    added search,skip,limit and page

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,19 +2,28 @@ import Product from '../models/Product.js'
 import products from '../products.json' assert {type:'json'}
 
 async function getAllProducts (req,res) {
-    const {featured,company,search,sort} = req.query
+    let {featured,company,search,sort,fields} = req.query
     const filter = Object.assign({}, 
         featured && { featured },
         company && { company },
         search && {name: {$regex: search,$options:'i'} }
         )
-    let product = await Product.find().sort('-price name') 
+    if(sort) {
+        let sortList = sort.split(",").join(" ")
+        sort = sortList || "-createdAt" 
+    } else if(fields) {
+        fields = fields.split(",").join(" ") 
+    }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    let product = await Product.find(filter).sort(sort).select(fields).limit(limit).skip(skip)
     // if(sort) {
     //     let multipleSort = sort.split(",").join(" ") 
     //     result = result.sort(multipleSort)
     // } 
     // const product = await result
-    res.json({product})
+    res.json({product,length:product.length})
 }
 async function sendData () {
     try {
