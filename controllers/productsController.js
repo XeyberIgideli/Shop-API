535ddee7730f4fa7ae08a3714a9f39a4834e3874
XeyberIgideli <d3facerx@gmail.com>
2023-05-11 18:07:05 +0400

    numeric filter

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,28 +1,60 @@
 import Product from '../models/Product.js'
-import products from '../products.json' assert {type:'json'}
+// import products from '../products.json' assert {type:'json'}
 
 async function getAllProducts (req,res) {
-    let {featured,company,search,sort,fields} = req.query
+    let {featured,company,search,sort,fields,numFilters} = req.query
+    const signList = {
+        '>':'$gt',
+        '>=': '$gte',
+        '=': '$eq',
+        '<':'$lt',
+        '<=': '$lte'
+    } 
+    // Filter operation for numeric filters
+    let filterOp = {}
+    for(let sign in signList) {
+        const splitted = numFilters.split(',')
+        
+        if(splitted.length === 1) {
+            if(numFilters.split(sign).length !== 1) {
+                const filterSign = signList[sign]
+                const name = numFilters.split(sign)[0]
+                filterOp[name] = {} 
+                filterOp[name][filterSign] = Number(numFilters.match(/\d+(?:\.\d+)?/)[0])
+            } 
+        } 
+         splitted.forEach(query => {
+            if(query.split(sign).length !== 1) {
+                const filterSign = signList[sign]  
+                const name = query.split(sign)[0]
+                filterOp[name] = {}              
+                filterOp[name][filterSign] = Number(query.match(/\d+(?:\.\d+)?/)[0])
+            } 
+         })
+    }    
+    
+    // Filter
     const filter = Object.assign({}, 
         featured && { featured },
         company && { company },
-        search && {name: {$regex: search,$options:'i'} }
-        )
+        search && {name: {$regex: search,$options:'i'} },
+        numFilters && filterOp
+    )  
+    // Checking    
     if(sort) {
         let sortList = sort.split(",").join(" ")
         sort = sortList || "-createdAt" 
     } else if(fields) {
         fields = fields.split(",").join(" ") 
     }
+    // Pagination
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || 10
     const skip = (page - 1) * limit
+
+    // DB Fetching
     let product = await Product.find(filter).sort(sort).select(fields).limit(limit).skip(skip)
-    // if(sort) {
-    //     let multipleSort = sort.split(",").join(" ") 
-    //     result = result.sort(multipleSort)
-    // } 
-    // const product = await result
+
     res.json({product,length:product.length})
 }
 async function sendData () {
